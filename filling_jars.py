07a0754147cf014befc56aa192f2cd1e6c904ca2
Bora Akaydin <bora.akaydin@wesystems.ag>
2026-04-20 18:16:59 +0000

    Filling jars solved.

---
+++ filling_jars.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY operations
+#
+
+def solve(n, operations):
+    # Write your code here
+    topl = 0;
+    for op in operations:
+        topl = topl + (op[1] - op[0] + 1) * op[2];
+    return topl // n;
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    operations = []
+
+    for _ in range(m):
+        operations.append(list(map(int, input().rstrip().split())))
+
+    result = solve(n, operations)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
